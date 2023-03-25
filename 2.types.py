101338acb518c9bef348f89083536facb491780a
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-03-25 20:01:29 +0530

    Add files via upload

---
+++ 2.types.py
@@ -0,0 +1,15 @@
+a="hello"
+b=3
+c=3.14
+d=True
+d=False
+e=[1,2,3,4,5]
+f=(1,2,3,4,5)
+g={1,2,3,4,5}
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
