74c8980d81eb9631b4d36ba67a733c741fc94e25
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-03-29 19:31:43 +0530

    Add files via upload

---
+++ 5.range step.py
@@ -0,0 +1,10 @@
+#range(Start,end, step)
+print('this are odd numbers from 1-10:')
+r=range(1,10,2)
+for value in r:
+  print(value)
+print('this are even numbers from 1-10:')
+r=range(0,10,2)
+for value in r:
+  print(value)
+    
