3b2aab651cb8b2faaf35f5bc6bee159d040f5740
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-04-14 18:58:57 +0530

    Add files via upload

---
+++ _16.count list.py
@@ -0,0 +1,6 @@
+#count= counting certain item
+l=['a','ab',2,3,4,7,3]
+print("count for a:",l.count('a'))
+print("count for ab:",l.count('ab'))
+print("count for 2:",l.count(2))
+print("count for 3:",l.count(3))
