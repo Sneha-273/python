6c5b8db954dba2e18c34749fa8de435a1f4dfd27
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-05-19 19:57:48 +0530

    Add files via upload

---
+++ _50.iter.py
@@ -0,0 +1,7 @@
+colour=("orange","pink","green","white")#touple
+c=iter(colour)
+print(next(c))   # iterator= it is object is contains countable number of values
+print(next(c))   #method= iter() and next()
+print(next(c))
+print(next(c))
+
