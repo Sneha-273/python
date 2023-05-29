d6fb188fa2df0e55f5436a1a93e6cc526ebfee1f
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-05-29 19:31:46 +0530

    Add files via upload

---
+++ _60.same variable scope.py
@@ -0,0 +1,9 @@
+a="Good morning"
+def fun():
+    a="Good afternoon"
+    print(a)
+fun()
+print(a)
+
+#if we use same named varable ,the python will treats them according
+# as a outside function=global and inside function=local
