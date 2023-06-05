110c07c6cfbcb48443c0812be43a5602e5d8d088
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-06-05 17:42:03 +0530

    Add files via upload

---
+++ _68.maths module.py
@@ -0,0 +1,10 @@
+import math
+#math.sqrt() finds squre root
+a=int(input("enter the number for finding squre root:"))
+print("Answer:",math.sqrt(a))
+#math.ceil() = returns next/upword complete integer
+b=float(input("enter the number for finding upword nearest number:"))
+print("Answer:",math.ceil(b))
+#math.floor()= returns back completed interger
+c=float(input("enter the number for finding upword nearest number:"))
+print("Answer:",math.floor(c))
