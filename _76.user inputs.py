45af8bd25c02fc864925d9aab317052daa416de4
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-06-28 18:54:28 +0530

    Add files via upload

---
+++ _76.user inputs.py
@@ -0,0 +1,5 @@
+a=input("enter character:")#python 3.6
+print("enterd characters are:"+a)
+print("enterd characters are:",a)#',': notice in output extra unessesory space get use
+#b=raw_input("enter string:")#python 2.7 use this function
+#print("entered string:",b)
