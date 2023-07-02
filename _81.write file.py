e92e0fb5c5650ccf7539fa0772c060989f99016d
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-07-02 20:11:41 +0530

    Add files via upload

---
+++ _81.write file.py
@@ -0,0 +1,5 @@
+f=open("myfile2.txt","w")# x : use mode to create new file
+f.write("hello!!")
+#read ():use to read file and its default mode
+#write():to create new file and write in file
+#append():to use write data in existing file in the ends without disturbing original data
