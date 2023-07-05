7c0e8d926c652047b73dbfe50b262cd50b39be43
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-07-05 19:37:04 +0530

    Add files via upload

---
+++ _85.array indexing.py
@@ -0,0 +1,4 @@
+import numpy as nm
+a=nm.array([1,2,3,4,5,6,7])
+print("addtion of array element:\n",a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6])
+print("multiplication of array element:\n",a[0]*a[1]*a[2]*a[3]*a[4]*a[5]*a[6])
