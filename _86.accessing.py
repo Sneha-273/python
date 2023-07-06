4d076bc93854f0bc9f08fdf915fddcebbd34d804
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-07-06 19:31:38 +0530

    Add files via upload

---
+++ _86.accessing.py
@@ -0,0 +1,5 @@
+import numpy as np
+a=np.array([[0,1,2,3,5],[6,7,8,9,10]])
+row=int(input("enter the row number:"))
+col=int(input("enter the column number:"))
+print("the position on",row,"and position on ",col,"is:",a[row,col])
