659aa2456906580a174ef8a3bbdc463bd6cd2aae
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-07-15 19:29:42 +0530

    101.filtering array.py

---
+++ 101.filtering array.py
@@ -0,0 +1,6 @@
+#in a filtering boolean index list use and only true values have existing in new array and
+#false values get ignored
+import numpy as np
+z=np.array([1,2,3,4])
+a=[True,True,False,False]
+print(z[a])
