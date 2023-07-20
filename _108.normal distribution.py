665996168bc85400fc2fcbdcd619a6459c0e35af
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-07-20 18:42:59 +0530

    Add files via upload

---
+++ _108.normal distribution.py
@@ -0,0 +1,9 @@
+from numpy import random
+a=random.normal(size=(1,3))
+print(a)
+#loc:mean np
+#scale:standard deviation (npq)
+#size:shape n*m
+b=random.normal(loc=10,scale=20,size=(2,2))
+print(b)
+#noraml Distribution:continuos i.e.heart beats
