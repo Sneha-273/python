500492c05e07cee732e5f825ed4181feb5f254ef
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-07-21 22:06:38 +0530

    Add files via upload

---
+++ _110.Binomial distribution.py
@@ -0,0 +1,10 @@
+#Binomial distribution:discrete, i.e.coin
+from numpy import random
+a=random.binomial(n=10,p=0.5,size=5)
+print(a)
+#n:no. of trails
+#p:probability first (0.5)
+#size:n*m
+#mean=np
+#S.D=sqrt(npq)
+#varience:npq
