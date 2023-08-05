fb0a22eb3f1aec57af90822849f446a74bbe5cf2
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-08-05 20:13:53 +0530

    Add files via upload

---
+++ _135.summations.py
@@ -0,0 +1,5 @@
+import numpy as np
+arr1=np.array([10,20,30,40,50])
+arr2=np.array([1,1,1,1,1])
+print('summation:',np.sum([arr1,arr2]))
+print('commulative:',np.cumsum([arr1,arr2]))
---
+++ _136.lowest common multiple.py
@@ -0,0 +1,7 @@
+import numpy as np
+a=24
+b=12
+print('lowest common multiple in two element:',np.lcm(a,b))
+print("\n------------------------------")
+arr1=np.array([2,14,20])
+print('lowest common multiple in array:',np.lcm.reduce(arr1))
