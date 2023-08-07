b3a2e61cdd8a0f989ba662bfc4da525d6b1af8f2
Snehal Alavekar <118888940+Sneha-273@users.noreply.github.com>
2023-08-07 21:16:03 +0530

    Add files via upload

---
+++ _137.greatest common denominator.py
@@ -0,0 +1,7 @@
+import numpy as np
+a=14
+b=55
+print('The GCD of numbers:',np.gcd(a,b))
+print('\n----------------------------')
+arr1=np.array([85,567,78,2,23,56])
+print('The GCD of the array element:',np.gcd.reduce(arr1))
